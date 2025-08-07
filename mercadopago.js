fcf02bf2850f01f5d940e6e263c261128e98a56a
Fabio Azevedo <fbbxo.dev@gmail.com>
2025-08-06 22:29:08 -0300

    Alterando mercadopago.js para testar o commit

--- mercadopago.js
+++ mercadopago.js
@@ -2,3 +2,12 @@ const m = "mercado pago!!!"
 const p = "pagamento"
 console.log(m, p);
 const d = "deu certo!!!"
+
+console.log("Debug")
+console.log(d)
+console.log("Debug Sexo")
+console.log(d)
+console.log("Debug")
+console.log(d)
+console.log("Debug")
+console.log(d)
